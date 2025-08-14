becc25678024743c7b313648402a4e6701eaa8b9
victor-huallpa <vech.600@gmail.com>
2025-08-14 10:02:08 -0500

    uso de rutas y inico automatico de la app

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+from markupsafe import escape
+
+#nombre del codigo determina la ubicacion de la carpeta raiz
+app = Flask(__name__)
+
+#usar plantillas html y redireccionar
+@app.route("/")# ers una plantilla
+def web_index():
+    #tempale nos ayuda a identificar la caroeta templates donde se encuetra los html o plantillas html
+    return render_template('index.html')
+
+@app.route("/nosotros")
+def web_nostros():
+    return render_template("nosotros.html")
+
+#url
+# @app.route("/")#decorador que ejecuta en la carpeta raiz y puedes manejar las rutas
+# def hello_world():
+#     return "Hello World!!!"
+
+@app.route("/inicio")
+def inicio():
+    return"""
+        <html>
+            <body>
+                <h1>Saludo bienvenido a inicio</h1>
+                <p>Esta vista pertenece a la pagiana inicio</p>
+            </body>
+        </html>
+    """
+
+#resive pararmetros dinamicos y los usa para su veneficio
+ruta = "/saludo/<nombre>/"
+@app.route(ruta)
+def saludo(nombre):
+    return f"Saludos {escape(nombre)}"
+
+#usando la misma plantilla para diferentes datos mandamos en la variable y ruta
+@app.route("/python")
+def curso_1():
+    #retornamos el archivo html y las varaibles que su usarasn en la pagina
+    return render_template(template_name_or_list="modificable.html",nombre="victor",curso="Flask")
+
+@app.route("/php")
+def curso_2():
+    #retornamos el archivo html y las varaibles que su usarasn en la pagina
+    return render_template(template_name_or_list="modificable.html",nombre="victor",curso="PHP")
+
+
+#ejecuta flask 
+if __name__ == "__main__":
+    # DEBUG = True
+    app.run()
+
